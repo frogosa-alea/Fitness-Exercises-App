866c57376f54d86b5deae404e07d2feb1c199d34
frogosa-alea <benitezalea.f@gmail.com>
2022-07-27 03:56:09 +0800

    Structuring folders

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <App/>
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
 )
